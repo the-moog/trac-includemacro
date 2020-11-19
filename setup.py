b9d976dfbf992d0f7d1e11a83c809c4241cfe615
Jason Morgan <jmorgan@sentec.co.uk>
2020-11-19 19:47:41 +0000

    Changed metadata

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from setuptools import setup
 
 setup(
     name='TracIncludeMacro',
-    version='3.0.0',
+    version='4.0.0',
     packages=['includemacro'],
 
     author='Noah Kantrowitz',
@@ -24,8 +24,8 @@ setup(
     description='Include the contents of external URLs and other Trac objects in a wiki page.',
     long_description=open(os.path.join(os.path.dirname(__file__), 'README')).read(),
     license='3-Clause BSD',
-    keywords='trac 0.11 plugin wiki include macro',
-    url='http://trac-hacks.org/wiki/IncludeMacro',
+    keywords='trac 1.4 plugin wiki include macro',
+    url='http://github.com/the-moog/trac-includemacro',
     classifiers=[
         'Framework :: Trac',
     ],
